b747a67ea93d61f04579d05b5ddd7b42157189ad
Pavel Kononov <woodsleaf@gmail.com>
2018-02-20 20:52:21 +0300

    Update createvhost.sh

--- createvhost.sh
+++ createvhost.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #bash snc.sh site user
-if [ -z $1 ] ; then 
+if [ -z $1 ] ; then
 echo «Do not set your domain name.»
 exit 1 ; fi
-if [ -z $2 ] ; then 
+if [ -z $2 ] ; then
 username='im'
 echo «Do not set your user name. Default name is ${username}» ; fi
 if [ -d ~/$1/ ]; then
@@ -14,9 +14,9 @@ exit 1; fi
 mkdir -p /home/$username/public_html/$1/public_html
 echo "127.0.0.1 "$1 | sudo tee -a /etc/hosts
 
-sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$1.tmp.conf
+sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-availa$
 
-sudo sed -e "s!\#ServerName www.example.com!ServerName ${1}!; s!DocumentRoot /var/www/html!DocumentRoot /home/${username}/public_html/${1}/public_html!" /etc/apache2/sites-available/$1.tmp.conf | sudo tee -a /etc/apache2/sites-available/$1.conf
+sudo sed -e "s!\#ServerName www.example.com!ServerName ${1}!; s!DocumentRoot /v$
 sudo rm /etc/apache2/sites-available/$1.tmp.conf
 
 sudo a2ensite $1
