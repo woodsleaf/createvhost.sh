80caba73b3f1453722f5bc513ee3489499874d05
Pavel Kononov <woodsleaf@gmail.com>
2018-02-20 20:48:14 +0300

    Update createvhost.sh

--- createvhost.sh
+++ createvhost.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-#sudo bash snc.sh site user
-if [ -z $1 ] ; then
+#bash snc.sh site user
+if [ -z $1 ] ; then 
 echo «Do not set your domain name.»
 exit 1 ; fi
-if [ -z $2 ] ; then
+if [ -z $2 ] ; then 
 username='im'
-echo «Do not set your user name.» ; fi
+echo «Do not set your user name. Default name is ${username}» ; fi
+if [ -d ~/$1/ ]; then
+echo '«Directory exist, script stoped.»'
+exit 1; fi
+
 
 mkdir -p /home/$username/public_html/$1/public_html
 echo "127.0.0.1 "$1 | sudo tee -a /etc/hosts
 
-cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$1.tmp.conf
+sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$1.tmp.conf
 
-sed -e "s!\#ServerName www.example.com!ServerName ${1}!; s!DocumentRoot /var/www/html!DocumentRoot /home/${username}/public_html/${1}/public_html!" /etc/apache2/sites-available/$1.tmp.conf > /etc/apache2/sites-available/$1.conf
-rm /etc/apache2/sites-available/$1.tmp.conf
+sudo sed -e "s!\#ServerName www.example.com!ServerName ${1}!; s!DocumentRoot /var/www/html!DocumentRoot /home/${username}/public_html/${1}/public_html!" /etc/apache2/sites-available/$1.tmp.conf | sudo tee -a /etc/apache2/sites-available/$1.conf
+sudo rm /etc/apache2/sites-available/$1.tmp.conf
 
-a2ensite $1
-chown $username:www-data -R /home/$username/public_html/$1
+sudo a2ensite $1
+sudo chown $username:www-data -R /home/$username/public_html/$1
 chmod -R 775 /home/$username/public_html/$1
+sudo service apache2 reload
